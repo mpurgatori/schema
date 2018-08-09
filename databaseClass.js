59b18a17ee00e06b4ac03ba98dde199e52868194
Mike Purgatori <mpurgatori@gmail.com>
2018-08-09 08:12:12 -0500

    initial

---
+++ databaseClass.js
@@ -0,0 +1,58 @@
+
+
+
+let data = {
+    users: {
+        ID1: {
+            name: "Alice",
+            email: "alice@email.com"
+        },
+        ID2: {
+            name: "Bob",
+            address: {city:"New York", country: "US"}
+        }
+    }
+};
+
+
+
+
+
+
+const offlineDatabase = ()=> {
+
+    let pathObject;
+
+    let ref = function(path) {
+        pathObject = path.split("/").reduce((data,i) => data[i], data);
+        data = data[path.split("/").reduce((data,i) => data[i], data)]
+        return this;
+    }
+
+    let push = function(path) {
+        pathObject = path.split("/").reduce((data,i) => data[i], data);
+        this.print();
+    }
+
+    let once = function(callback) {
+        callback(pathObject)
+    }
+
+    let remove = function() {
+        delete data[pathObject];
+    }
+
+    return {
+        ref,
+        once,
+        remove
+    };
+  };
+
+
+
+let x = offlineDatabase();
+
+console.log('!@!@',data)
+console.log(x.ref('users/ID2/name').remove())
+console.log(')()',data)
